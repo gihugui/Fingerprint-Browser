adbaca3cea583ff3178594838d5d4f614e835506
hg <812376073@qq.com>
2024-01-09 14:39:38 +0800

    1

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "欢迎使用指纹浏览器！" << std::endl;
-    std::cout << "这是一个基于C++开发的浏览器工具示例。" << std::endl;
-
-    // 在这里添加您的功能代码
+    std::cout << "仅展示图片，无代码！" << std::endl;
+	std::cout << "如需要技术支持，请联系作者！" << std::endl;
+    // 在这里添加您的功能代码 
     return 0;
 }
